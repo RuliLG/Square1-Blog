2808a090b924d76c3b4203705d27fde35e47697e
Raúl López <hi@raullg.com>
2021-12-31 19:44:57 +0000

    feat: noindex post if it's not published

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -19,9 +19,18 @@ class BlogController extends Controller
     public function show($id, Request $request)
     {
         // If the URL is signed, we can display the post even if it's not published
+
         $service = new BlogPostService();
-        return view('blog-post', [
-            'post' => URL::hasValidSignature($request) ? $service->forceShow($id) : $service->show($id),
-        ]);
+        $post = URL::hasValidSignature($request) ? $service->forceShow($id) : $service->show($id);
+        $headers = [];
+        if (!$post->is_published) {
+            $headers['X-Robots-Tag'] = 'noindex';
+        }
+
+        return response()
+            ->view('blog-post', [
+                'post' => $post,
+            ])
+            ->withHeaders($headers);
     }
 }
