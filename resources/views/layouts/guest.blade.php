652f165b0c388eac1793817de258c670fb3ea1f5
Raúl López <hi@raullg.com>
2021-12-31 15:16:38 +0000

    feat: update logo to link

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -25,11 +25,11 @@
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div class="flex justify-between h-16">
                 <div class="flex">
-                    <div class="flex-shrink-0 flex items-center">
+                    <a href="{{ route('home') }}" class="flex-shrink-0 flex items-center">
                         {{-- TODO: Change logo to a proper image --}}
                         <img class="block lg:hidden h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow">
                         <img class="hidden lg:block h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow">
-                    </div>
+                    </a>
                 </div>
                 <div class="flex items-center">
                     <div class="flex-shrink-0">
